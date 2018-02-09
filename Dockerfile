c7304b94a831b33aebebccacf00b439072a29d40
bortes <eduardoflorencio@brq.com>
2018-02-09 08:27:26 -0200

    Change shell script to work properly.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine
 
+# docker entrypoint
 ADD docker-entrypoint.sh /
 
+# setting entrypoint
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,10 @@
 env | while IFS='=' read -r env_name env_value ; do
 
   # transform to lowercase
-  declare -l env_name
+  env_name=$(echo $env_name | tr '[:upper:]' '[:lower:]')
 
   # check if variable starts with sysctl
-  if [[ $env_name == "sysctl"* ]]; then
+  if [[ "$env_name" == sysctl* && ! -z $env_value ]]; then
 
     # extract key name
     key=${env_name#*_}
@@ -16,7 +16,7 @@ env | while IFS='=' read -r env_name env_value ; do
     result=$(sysctl $key 2>&1)
 
     # check return code from sysctl
-    if [[ $? -eq "0" && ! -z $env_value ]]; then
+    if [[ $? -eq "0" ]]; then
 
       # get current value
       old_value=${result#*=}
@@ -24,12 +24,18 @@ env | while IFS='=' read -r env_name env_value ; do
       # setting new value
       new_value=$env_value
 
-      # changing value 
-      sysctl --write $key=$new_value
+      # check values
+      if [ old_value != new_value ]; then
+        
+        # changing value 
+        sysctl --write $key=$new_value
 
-      # result
-      if [[ $? -eq "0" ]]; then
-        echo $(date) - $USER - INFO  - Changing value of sysctl key $key from $old_value to $new_value.
+        # notify changing
+        echo $(date) - $USER - INFO  - changed successfully. sysctl key $key value from $old_value to $new_value.
+      else
+
+        # notify not changing
+        echo $(date) - $USER - WARN  - unchanged property. sysctl key $key already has $old_value.
       fi
 
     fi
